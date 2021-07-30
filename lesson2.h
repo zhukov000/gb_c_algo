6be5cf5284744104ae5dd085dca7a7fee58acaa7
azhukov <zhukov000@gmail.com>
2021-07-30 17:21:08 +0300

    Lesson2

---
+++ lesson2.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void helloWorld();
+
+void helloWorld() {
+  printf("Hello, world!");
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+* azhukov
+*/
+
+#include"lesson2.h"
+
+int main() {
+
+  helloWorld(); // lesson 2
+
+  return 0;
+}
