b30bf80e6c0c1b89c92e93083665ef4939beb97d
azhukov <zhukov000@gmail.com>
2021-08-06 17:28:29 +0300

    Lesson 6

--- main.c
+++ main.c
@@ -10,7 +10,19 @@ int main () {
   printArr(arr, n);
   printf("\n\n");
 
-  // example bubble 2d sort
+  // example bubble 1d sort for 2d array -
+  // for static array can using 1d array functions
+  int arr1[3][3] = {1,9,2,5,7,6,4,3,8};
+  bubbleSort(arr1, n);
+  for(unsigned i=0; i<3; ++i) {
+    for(unsigned j=0; j<3; ++j) {
+      printf("%2d ", arr1[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+
+  // example bubble 2d sort dynamic array
   int** arr2 = init2dArr(n, n);
   fill2dArr(arr2, n, n);
   bubble2dSort(arr2, n, n);
