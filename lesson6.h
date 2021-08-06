11e35003341cb5349e1cc23a07379ecfbf2ac141
azhukov <zhukov000@gmail.com>
2021-08-06 08:22:33 +0300

    Lesson 6

--- lesson6.h
+++ lesson6.h
@@ -3,8 +3,11 @@
 
 #include<stdio.h>
 #include<math.h>
+#include<malloc.h>
+#include<time.h>
 
 #define swap(x,y) pSwap(&x, &y)
+#define E(a,i,r) a[(i)/(r)][(i)%(r)]
 
 void pSwap(int *x, int *y) {
   int t = *x;
@@ -18,11 +21,55 @@ void printArr(const int *arr, unsigned size) {
   }
 }
 
+int** init2dArr(unsigned rows, unsigned cols) {
+  int** arr = (int**)malloc(sizeof(int*) * rows);
+  for(unsigned irow=0; irow<rows; ++irow) {
+    arr[irow] = (int*)calloc(sizeof(int), cols);
+  }
+  return arr;
+}
+
+void free2dArr(int** arr, unsigned rows) {
+  for(unsigned irow=0; irow<rows; ++irow) {
+    free(arr[irow]);
+  }
+  free(arr);
+}
+
+void fill2dArr(int** arr, unsigned rows, unsigned cols) {
+  srand(time(0));
+  for(unsigned irow=0; irow<rows; ++irow) {
+    for(unsigned icol=0; icol<cols; ++icol) {
+        arr[irow][icol] = rand() % 100;
+    }
+  }
+}
+
+void print2dArr(const int **arr, unsigned rows, unsigned cols) {
+  for(unsigned irow=0; irow<rows; ++irow) {
+    for(unsigned icol=0; icol<cols; ++icol) {
+      printf("%3d ", arr[irow][icol]);
+    }
+    printf("\n");
+  }
+}
+
 void bubbleSort(int *arr, unsigned size) {
   for(unsigned i=(int)size-1; i>0; --i) {
     for(unsigned j=0; j<i; ++j) {
       if (arr[j] > arr[j+1]) {
         swap(arr[j], arr[j+1]);
+      }
+    }
+  }
+}
+
+void bubble2dSort(int **arr, unsigned r, unsigned c) {
+  unsigned size = r * c;
+  for(unsigned i=(int)size-1; i>0; --i) {
+    for(unsigned j=0; j<i; ++j) {
+      if (E(arr,j,r) > E(arr,j+1,r)) {
+        swap(E(arr,j,r), E(arr,j+1,r));
       }
     }
   }
--- main.c
+++ main.c
@@ -3,11 +3,19 @@
 
 int main () {
 
-  // example bubble sort
+  // example bubble 1d sort
   const unsigned n = 9;
   int arr[] = {1,9,2,5,7,6,4,3,8};
   bubbleSort(arr, n);
   printArr(arr, n);
+  printf("\n\n");
+
+  // example bubble 2d sort
+  int** arr2 = init2dArr(n, n);
+  fill2dArr(arr2, n, n);
+  bubble2dSort(arr2, n, n);
+  print2dArr(arr2, n, n);
+  free2dArr(arr2, n);
 
   // Trabb Pardo Knuth algorithm
   tpk_algo();
