c488566461bf4d480385d8d1e54143a80ab30d7a
azhukov <zhukov000@gmail.com>
2021-08-06 04:32:17 +0300

    Time check for prime implementation

--- lesson3.h
+++ lesson3.h
@@ -1,6 +1,40 @@
+#ifndef LESSON3_GB
+#define LESSON3_GB
 
+#include <math.h>
+
+//
 int isPrime(unsigned x) {
   unsigned d = 2;
   while ((d * d <= x) && (x % d != 0)) ++d;
   return (x > 1) && (d * d > x);
 }
+
+//
+int isPrimeSqrt(int x) {
+  int last = (int) sqrt(x);
+  for(int i = 2; i<= last; ++i) {
+    if (x % i == 0)
+      return 0;
+  }
+  return 1;
+}
+
+//
+int prime_number_student(int *value)
+{
+  if(*value > 1 && *value < 3)
+    return 1;
+
+  if(*value < 2 || *value % 2 == 0)
+    return 0;
+
+  int v = (int)sqrt(*value);
+  for(int i=3; i<=v; i+=2) {
+    if ( *value % i == 0 )
+      return 0;
+  }
+  return 1;
+}
+
+#endif
--- main.c
+++ main.c
@@ -6,38 +6,53 @@
 #include"lesson3.h"
 #include"lesson4.h"
 
+#include"time_measure.h"
+
+void func1() {
+  int x[2] = {1e9+7, 2147483647};
+  for(unsigned i=0; i<1000; ++i) {
+    isPrime(x[i&1]);
+  }
+}
+
+void func2() {
+  int x[2] = {1e9+7, 2147483647};
+  for(unsigned i=0; i<1000; ++i) {
+    isPrimeSqrt(x[i&1]);
+  }
+}
+
+void func3() {
+  int x[2] = {1e9+7, 2147483647};
+  for(unsigned i=0; i<1000; ++i) {
+    prime_number_student(&(x[i&1]));
+  }
+}
+
 int main() {
 
-  // lesson 4
-  printf("\n");
-  unsigned n = 123;
-  for(unsigned base = 2; base < 17; ++base) {
-    char buffer[33] = "0";
-    conv(n, base, buffer, 0);
-    printf("%u in %u = %s\n", n, base, buffer);
+  double sr = 0;
+  unsigned n = 30;
+
+  for(unsigned test=0; test<n; ++test) {
+    double r = get_clock_diff(func1);
+    sr += r;
   }
-  printf("\n");
-  for(int i=-5; i<=5; ++i) {
-    printf("2^%d = %5.3lf\n", i, power(2., i));
+  printf("Function i*i <= n - average: %.9lf\n", sr / n);
+
+  sr = 0;
+  for(unsigned test=0; test<n; ++test) {
+    double r = get_clock_diff(func2);
+    sr += r;
   }
-  printf("\n");
+  printf("\nFunction sqrt average: %.9lf\n", sr / n);
 
-  const unsigned N = 13;
-  const unsigned M = 25;
-  int board[N][M];
-
-  // random initialize
-  init01(N, M, board);
-  // print
-  print(N, M, board);
-  // search
-  printf("\n");
-
-  unsigned ans = search_r(N-1, M-1, N, M, board);
-  printf("Result of recursive search = %d\n", ans); // slow
-
-  ans = search_dp(N, M, board);
-  printf("Result of search with using DP optimization = %d\n", ans); // fast
+  sr = 0;
+  for(unsigned test=0; test<n; ++test) {
+    double r = get_clock_diff(func3);
+    sr += r;
+  }
+  printf("\nStep +2 average: %.9lf\n", sr / n);
 
   return 0;
 }
